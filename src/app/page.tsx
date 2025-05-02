aee685b9df2afc74cf9d9de9d94ca3cebd49275c
thijnmens <thijnsmulders04@gmail.com>
2025-05-02 13:57:47 +0200

    Fix font not showing up if the font is not installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 // import Searchbar from '@/components/searchbar';
 
+import localFont from 'next/font/local';
+
+const crashbow = localFont({
+    src: '../../public/fonts/crashbow.otf',
+})
+
 export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-16 items-center h-full w-full'>
-          <h1 className='font-crashbow text-9xl bg-gradient-to-r w-fit px-6 py-4 from-primary to-secondary text-transparent bg-clip-text'>Anime NL</h1>
+          <h1 className={`${crashbow.className} text-9xl bg-gradient-to-r w-fit px-6 py-4 from-primary to-secondary text-transparent bg-clip-text`}>Anime NL</h1>
           <p className='text-xl underline'>Jouw connectie tot de japanse otaku markt</p>
           <h2>Opent binnenkort / Opens soon</h2>
           {/*<Searchbar />*/}
