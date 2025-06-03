d08c9d4decce3a2ef89c120d4fc1f56d5e7915ef
thijnmens <thijnsmulders04@gmail.com>
2025-06-03 21:45:40 +0200

    Mollie webhook test

---
+++ src/app/api/mollie/webhook/route.ts
@@ -0,0 +1,47 @@
+import * as crypto from 'node:crypto';
+
+export async function POST(req: Request) {
+	// Request has X-Mollie-Signature header
+	if (!req.headers.has('X-Mollie-Signature'))
+		return new Response('', {
+			status: 401
+		});
+
+	// MOLLIE_WEBHOOK_SECRET is set
+	if (!process.env.MOLLIE_WEBHOOK_SECRET)
+		return new Response('MOLLIE_WEBHOOK_SECRET env not defined', {
+			status: 500
+		});
+
+	// Generate hashing object
+	const hmac = crypto.createHmac('sha256', process.env.MOLLIE_WEBHOOK_SECRET);
+
+	// Read body
+	const bodyReader = req.body?.getReader();
+
+	// Generate hash
+	hmac.update(bodyReader?.read().toString() ?? '');
+	console.log(bodyReader?.read().toString());
+
+	// Release body reader
+	bodyReader?.releaseLock();
+
+	console.log(process.env.MOLLIE_WEBHOOK_SECRET);
+	console.log(req.headers.get('X-Mollie-Signature'));
+	console.log(hmac.digest('hex'));
+	console.log(req.headers.get('X-Mollie-Signature') != hmac.digest('hex'));
+
+	// Check if hex values match
+	if (req.headers.get('X-Mollie-Signature') != hmac.digest('hex')) {
+		return new Response('', {
+			status: 401
+		});
+	}
+
+	const data = await req.json();
+	console.log(data);
+
+	return new Response('', {
+		status: 200
+	});
+}
