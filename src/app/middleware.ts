2b477e3025481767e3a97fef21f5d5dac2dcbcfc
thijnmens <thijnsmulders04@gmail.com>
2025-06-05 18:43:44 +0200

    Prettier run

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,9 +4,9 @@ import { NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
 	const headers = new Headers(request.headers);
 	headers.set('x-current-path', request.nextUrl.pathname);
-	return NextResponse.next({ headers });
+	return NextResponse.next({headers});
 }
 
 export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
