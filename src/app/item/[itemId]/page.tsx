354dffe5444dd9dce0f91bf9a5e56c936560ead5
thijnmens <thijnsmulders04@gmail.com>
2025-06-07 22:01:22 +0200

    Add metadata for item page

--- src/app/item/[itemId]/page.tsx
+++ src/app/item/[itemId]/page.tsx
@@ -2,15 +2,37 @@ import ErpNextHelper from '@/app/Helpers/ErpNextHelper';
 import ErrorPage from '@/components/errorPage';
 import ItemInfo from '@/components/itemInfo';
 import NotFoundNotif from '@/components/notFoundNotif';
+import { Metadata } from 'next';
 
-export default async function ItemPage({ params }: { params: Promise<{ itemId: string }> }) {
-	const { itemId } = await params;
+type Props = {
+	params: Promise<{ itemId: string }>;
+}
+
+export async function generateMetadata(
+	{params}: Props
+): Promise<Metadata> {
+	const {itemId} = await params;
+
+	const item = await ErpNextHelper.getItemById(itemId);
+
+	return {
+		title: item?.item_name ?? 'Niet Gevonden',
+		description: item?.description,
+		openGraph: {
+			images: item?.image
+		}
+	};
+
+}
+
+export default async function ItemPage({params}: Props) {
+	const {itemId} = await params;
 
 	if (!Number(itemId)) {
 		return (
 			<div className="dark mx-8 sm:mx-16 pt-16 min-h-screen font-[family-name:var(--font-geist-sans)]">
 				<main className="flex flex-col gap-16 items-center h-full w-full">
-					<NotFoundNotif />
+					<NotFoundNotif/>
 				</main>
 			</div>
 		);
@@ -19,7 +41,7 @@ export default async function ItemPage({ params }: { params: Promise<{ itemId: s
 	const item = await ErpNextHelper.getItemById(itemId);
 
 	if (!item) {
-		return <ErrorPage error="Item could not be found." />;
+		return <ErrorPage error="Item could not be found."/>;
 	}
 
 	const images = await ErpNextHelper.getImagesForItem(itemId);
@@ -27,7 +49,7 @@ export default async function ItemPage({ params }: { params: Promise<{ itemId: s
 	return (
 		<div className="dark mx-8 sm:mx-16 pt-16 min-h-screen font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-16 items-center h-full w-full">
-				<ItemInfo item={item} images={images?.sort() ?? []} />
+				<ItemInfo item={item} images={images?.sort() ?? []}/>
 			</main>
 		</div>
 	);
